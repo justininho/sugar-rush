2f045fde4f5040de007d71b111b7f88f2721ab6e
Justin <github@jlri.net>
2024-10-12 16:53:04 -0400

    fsm

--- player/player.gd
+++ player/player.gd
@@ -1,84 +1,163 @@
 extends CharacterBody2D
 
-const WALK_SPEED = 250.0#300.0
+const IDLE_LERP_AMOUNT := 0.25
+const WALK_SPEED = 250.0 # 300.0
 const RUN_SPEED = WALK_SPEED * 1.25
 const ACCELERATION_SPEED = WALK_SPEED * 6.0
-const JUMP_VELOCITY = -350.0#-400.0
-const MAX_JUMP_HOLD_TIME = 0.2  # Maximum time (seconds) to hold the jump for extra height
-const JUMP_HOLD_FORCE = -300.0  # Extra upward force applied when jump is held
+const JUMP_VELOCITY = -350.0 # -400.0
+const MAX_JUMP_HOLD_TIME = 0.2 # Maximum time (seconds) to hold the jump for extra height
+const JUMP_HOLD_FORCE = -300.0 # Extra upward force applied when jump is held
 
-const WALL_GLIDE_SPEED := 50.0
-const WALL_GLIDE_LERP_AMOUNT := 0.1
+const WALL_SLIDE_SPEED := 50.0
+const WALL_SLIDE_LERP_AMOUNT := 0.1
 
 @onready var animated_sprite := $AnimatedSprite2D as AnimatedSprite2D
+@onready var wall_slide_buffer_timer := $WallSlideBufferTimer as Timer
 
-var jump_time_remaining := 0.0  # How much time is left to add extra force
-var is_running := false
+signal state_change(state_string: String)
 
+var jump_time_remaining := 0.0 # How much time is left to add extra force
+
+enum PlayerState {IDLE, WALKING, RUNNING, JUMPING, FALLING, WALL_SLIDE}
+var current_state := PlayerState.IDLE
+
+func change_state(new_state: PlayerState):
+		if current_state == new_state:
+				return
+		print("Changing state from %s to %s" % [get_player_state_string(current_state), get_player_state_string(new_state)])
+		current_state = new_state
+		state_change.emit(get_player_state_string(current_state))
+
+func get_player_state_string(state: PlayerState) -> String:
+	match state:
+		PlayerState.IDLE:
+			return "idle"
+		PlayerState.WALKING:
+			return "walking"
+		PlayerState.RUNNING:
+			return "running"
+		PlayerState.JUMPING:
+			return "jumping"
+		PlayerState.FALLING:
+			return "falling"
+		PlayerState.WALL_SLIDE:
+			return "wall_slide"
+	return ""
+
+func update_state() -> void:
+	match current_state:
+		PlayerState.IDLE:
+			if has_direction():
+				change_state(PlayerState.WALKING)
+			elif Input.is_action_pressed("run"):
+				change_state(PlayerState.RUNNING)
+			elif Input.is_action_pressed("jump"):
+				change_state(PlayerState.JUMPING)
+		PlayerState.WALKING:
+			if not has_direction():
+				change_state(PlayerState.IDLE)
+			elif Input.is_action_pressed("run"):
+				change_state(PlayerState.RUNNING)
+			elif Input.is_action_pressed("jump"):
+				change_state(PlayerState.JUMPING)
+		PlayerState.RUNNING:
+			if not has_direction():
+				change_state(PlayerState.IDLE)
+			elif not Input.is_action_pressed("run"):
+				change_state(PlayerState.WALKING)
+			elif Input.is_action_pressed("jump"):
+				change_state(PlayerState.JUMPING)
+		PlayerState.JUMPING:
+			if is_on_floor_only():
+				if has_direction():
+					change_state(PlayerState.WALKING)
+				else:
+					change_state(PlayerState.IDLE)
+			elif Input.is_action_pressed("jump"):
+				change_state(PlayerState.JUMPING)
+			elif is_on_wall_only():
+				change_state(PlayerState.WALL_SLIDE)
+			elif velocity.y > 0.0:
+				change_state(PlayerState.FALLING)
+		PlayerState.FALLING:
+			if is_on_floor_only():
+				if has_direction():
+					change_state(PlayerState.WALKING)
+				else:
+					change_state(PlayerState.IDLE)
+			elif is_on_wall() and velocity.y > 0.0:
+				change_state(PlayerState.WALL_SLIDE)
+		PlayerState.WALL_SLIDE:
+				if is_on_floor_only():
+					if has_direction():
+						change_state(PlayerState.WALKING)
+					else:
+						change_state(PlayerState.IDLE)
+				elif not touching_wall() and velocity.y > 0.0:
+					change_state(PlayerState.FALLING)
+	
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	apply_gravity(delta)
+	handle_state(delta)
+	update_state()
+	move_and_slide()
+	animated_sprite.play(get_player_state_string(current_state))
+
+func apply_gravity(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
-	if is_on_wall_only():
-		velocity.y = lerp(velocity.y, WALL_GLIDE_SPEED, WALL_GLIDE_LERP_AMOUNT)
 
-	# walk or run
-	var speed := WALK_SPEED
-	if Input.is_action_pressed("run"):
-		is_running = true
-		speed = RUN_SPEED
-	else:
-		is_running = false
+func has_direction() -> bool:
+	return Input.get_axis("move_left", "move_right") != 0
+	
+@onready var wall_raycast = $WallRaycast
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump"):
-		try_jump()
+func touching_wall() -> bool:
+	return wall_raycast.is_colliding()
 		
+func handle_state(delta: float) -> void:
+	match current_state:
+		PlayerState.IDLE:
+			handle_idle()
+		PlayerState.WALKING:
+			handle_movement(delta, WALK_SPEED)
+		PlayerState.RUNNING:
+			handle_movement(delta, RUN_SPEED)
+		PlayerState.JUMPING:
+			handle_jump(delta)
+		PlayerState.FALLING:
+			handle_movement(delta, WALK_SPEED)
+		PlayerState.WALL_SLIDE:
+			handle_wall_slide()
+
+func handle_idle() -> void:
+	#print_debug("idle", "velocity.x: ", velocity.x)
+	velocity.x = lerp(velocity.x, 0.0, IDLE_LERP_AMOUNT)
+
+func handle_movement(delta: float, speed: float) -> void:
+	var direction := Input.get_axis("move_left", "move_right");
+	if absf(direction) > 0.0:
+		animated_sprite.flip_h = direction < 0
+	# print_debug("speed", speed, "direction", direction)
+	# print_debug("From: ", velocity.x, " To: ", direction, " Delta: ", ACCELERATION_SPEED * delta)
+	velocity.x = move_toward(velocity.x, direction * speed, ACCELERATION_SPEED * delta)
+	
+	# Handles initial jump and sets the jump hold time.
+func handle_jump(delta: float) -> void:
+	if is_on_floor():
+		velocity.y = JUMP_VELOCITY # Initial jump velocity
+		jump_time_remaining = MAX_JUMP_HOLD_TIME # Allow extra force for a limited time
+	
+	#	move in the air	
+	handle_movement(delta, WALK_SPEED)
+	
 	# Apply extra jump force if the jump button is held and there is time left.
-	print_debug("jump: ", Input.is_action_pressed("jump"), " jump time remaining: ", jump_time_remaining > 0.0)
+	# print_debug("jump: ", Input.is_action_pressed("jump"), " jump time remaining: ", jump_time_remaining > 0.0)
+	# todo test this
 	if Input.is_action_pressed("jump") and jump_time_remaining > 0.0:
 		velocity.y += JUMP_HOLD_FORCE * delta
 		jump_time_remaining -= delta
-	
-	var direction := Input.get_axis("walk_left", "walk_right") * speed;
-	print_debug("From: ", velocity.x, " To: ", direction, " Delta: ", ACCELERATION_SPEED * delta)
-	velocity.x = move_toward(velocity.x, direction, ACCELERATION_SPEED * delta)
-
-	move_and_slide()
-	
-	var animation := get_new_animation(direction)
-	animated_sprite.play(animation)
-
-# Handles initial jump and sets the jump hold time.
-func try_jump() -> void: 
-	if is_on_floor():
-		velocity.y = JUMP_VELOCITY  # Initial jump velocity
-		jump_time_remaining = MAX_JUMP_HOLD_TIME  # Allow extra force for a limited time
 		
-func get_new_animation(direction: float) -> String:
-	if absf(direction) > 0.0:
-		animated_sprite.flip_h = direction < 0
-	
-	if is_on_floor():
-		return _get_ground_animation()
-	else:
-		return _get_air_animation(direction)
-
-func _get_ground_animation() -> String:
-	if absf(velocity.x) > 0.1:
-		if is_running:
-			return "running"
-		else:
-			return "walking"
-	return "idle"
-
-func _get_air_animation(direction: float) -> String:
-#	pressed against wall
-	if is_on_wall_only():
-		#animated_sprite.flip_h = !animated_sprite.flip_h
-		return "wall"
-	elif velocity.y >= 0.0:
-		return "falling"
-	else:
-		return "jumping"
+func handle_wall_slide() -> void:
+	if is_on_wall():
+		velocity.y = lerp(velocity.y, WALL_SLIDE_SPEED, WALL_SLIDE_LERP_AMOUNT)
